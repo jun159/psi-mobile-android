60f5a223337f8915e8e8e9c3fc11257e3ed8d45d
jun159 <baojun159@gmail.com>
2017-09-08 23:43:14 +0800

    Get latitude and longtitudes for Singapore to be shown on map

--- app/src/main/java/sg/edu/nus/baojun/psy/activity/MainActivity.java
+++ app/src/main/java/sg/edu/nus/baojun/psy/activity/MainActivity.java
@@ -11,8 +11,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import sg.edu.nus.baojun.psy.R;
 
@@ -21,6 +21,14 @@ public class MainActivity extends PsyActionBarActivity implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         GoogleMap.OnMarkerClickListener {
+
+    private static final double SINGAPORE_LATITUDE = 1.290270;
+    private static final double SINGAPORE_LONGTITUDE = 103.851959;
+    private static final double SINGAPORE_SOUTH_LATITUDE = 1.149600;
+    private static final double SINGAPORE_WEST_LATITUDE = 103.594000;
+    private static final double SINGAPORE_NORTH_LATITUDE = 1.478400;
+    private static final double SINGAPORE_EAST_LATITUDE = 104.094500;
+    private static final float ZOOM = 10.3F;
 
     private GoogleMap map;
 
@@ -37,10 +45,14 @@ public class MainActivity extends PsyActionBarActivity implements
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+        LatLng latLng = new LatLng(SINGAPORE_LATITUDE, SINGAPORE_LONGTITUDE);
+        LatLngBounds latLngBounds = new LatLngBounds(
+                new LatLng(SINGAPORE_SOUTH_LATITUDE, SINGAPORE_WEST_LATITUDE),
+                new LatLng(SINGAPORE_NORTH_LATITUDE, SINGAPORE_EAST_LATITUDE));
 
-        LatLng sydney = new LatLng(-34, 151);
-        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, ZOOM));
+        googleMap.setLatLngBoundsForCameraTarget(latLngBounds);
+        googleMap.setOnMarkerClickListener(this);
     }
 
     @Override
@@ -50,12 +62,12 @@ public class MainActivity extends PsyActionBarActivity implements
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        handleNetworkFailure();
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        handleNetworkFailure();
     }
 
     @Override
