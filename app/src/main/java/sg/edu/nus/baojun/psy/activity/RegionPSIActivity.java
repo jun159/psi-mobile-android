d08e2eb31288fe9b08132b390d8b034cb860df45
jun159 <baojun159@gmail.com>
2017-09-09 17:13:36 +0800

    Displayed sub index with gradiant scale

--- app/src/main/java/sg/edu/nus/baojun/psy/activity/RegionPSIActivity.java
+++ app/src/main/java/sg/edu/nus/baojun/psy/activity/RegionPSIActivity.java
@@ -1,11 +1,13 @@
 package sg.edu.nus.baojun.psy.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.RelativeSizeSpan;
 import android.text.style.SubscriptSpan;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -40,44 +42,47 @@ public class RegionPSIActivity extends PsyActionBarActivity {
     @BindView(R.id.text_psi_status)
     TextView textPSIStatus;
 
-    @BindView(R.id.progress_pm10_sub_index)
-    ProgressBar progressPm10SubIndex;
-    @BindView(R.id.progress_pm25_sub_index)
-    ProgressBar progressPm25SubIndex;
-    @BindView(R.id.progress_o3_sub_index)
-    ProgressBar progressO3SubIndex;
-    @BindView(R.id.progress_co_sub_index)
-    ProgressBar progressCoSubIndex;
-    @BindView(R.id.progress_so2_sub_index)
-    ProgressBar progressSo2SubIndex;
-    @BindView(R.id.progress_no2_one_hour_max)
-    ProgressBar progressNo2OneHourMax;
+    @BindView(R.id.icon_status)
+    TextView iconStatus;
 
-    @BindView(R.id.label_pm10_sub_index)
-    TextView labelPm10SubIndex;
-    @BindView(R.id.label_pm25_sub_index)
-    TextView labelPm25SubIndex;
-    @BindView(R.id.label_o3_sub_index)
-    TextView labelO3SubIndex;
-    @BindView(R.id.label_co_sub_index)
-    TextView labelCoSubIndex;
-    @BindView(R.id.label_so2_sub_index)
-    TextView labelSo2SubIndex;
-    @BindView(R.id.label_no2_sub_index)
-    TextView labelNo2SubIndex;
+    @BindView(R.id.label_pm10)
+    TextView labelPm10;
+    @BindView(R.id.label_pm25)
+    TextView labelPm25;
+    @BindView(R.id.label_o3)
+    TextView labelO3;
+    @BindView(R.id.label_co)
+    TextView labelCo;
+    @BindView(R.id.label_so2)
+    TextView labelSo2;
+    @BindView(R.id.label_no2)
+    TextView labelNo2;
 
-    @BindView(R.id.text_pm10_sub_index)
-    TextView textPm10SubIndex;
-    @BindView(R.id.text_pm25_sub_index)
-    TextView textPm25SubIndex;
-    @BindView(R.id.text_o3_sub_index)
-    TextView textO3SubIndex;
-    @BindView(R.id.text_co_sub_index)
-    TextView textCoSubIndex;
-    @BindView(R.id.text_so2_sub_index)
-    TextView textSo2SubIndex;
-    @BindView(R.id.text_no2_one_hour_max)
-    TextView textNo2OneHourMax;
+    @BindView(R.id.text_pm10_before)
+    TextView textPm10Before;
+    @BindView(R.id.text_pm25_before)
+    TextView textPm25Before;
+    @BindView(R.id.text_o3_before)
+    TextView textO3Before;
+    @BindView(R.id.text_co_before)
+    TextView textCoBefore;
+    @BindView(R.id.text_so2_before)
+    TextView textSo2Before;
+    @BindView(R.id.text_no2_before)
+    TextView textNo2Before;
+
+    @BindView(R.id.text_pm10_after)
+    TextView textPm10After;
+    @BindView(R.id.text_pm25_after)
+    TextView textPm25After;
+    @BindView(R.id.text_o3_after)
+    TextView textO3After;
+    @BindView(R.id.text_co_after)
+    TextView textCoAfter;
+    @BindView(R.id.text_so2_after)
+    TextView textSo2After;
+    @BindView(R.id.text_no2_after)
+    TextView textNo2After;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,6 +105,7 @@ public class RegionPSIActivity extends PsyActionBarActivity {
             progressStatus.setProgress((int) (psi * PERCENTAGE) / VALUE_HAZARDOUS);
             textPSIRegion.setText(regionReadings.getPsiTwentyFourHourly());
             textPSIStatus.setText(String.format(getString(R.string.region_psi_status), determineStatus(psi)));
+            iconStatus.setBackgroundResource(getHealthStatusDrawable(psi));
             setPSIValues(regionReadings);
         } else {
             activityTitle.setText(getString(R.string.title_singapore_psi));
@@ -137,29 +143,48 @@ public class RegionPSIActivity extends PsyActionBarActivity {
     }
 
     private void setPSIValues(PSIRegionReadings regionReadings) {
-        labelPm10SubIndex.setText(superSubScript(getString(R.string.pm10_sub_index), SUBSCRIPT_TYPE_A));
-        progressPm10SubIndex.setProgress((int) Double.parseDouble(regionReadings.getPm10SubIndex()) * PERCENTAGE / VALUE_HAZARDOUS);
-        textPm10SubIndex.setText(regionReadings.getPm10SubIndex());
+        labelPm10.setText(superSubScript(getString(R.string.pm10_sub_index), SUBSCRIPT_TYPE_A));
+        setCurrentPsiLevel(textPm10Before, textPm10After, Float.parseFloat(regionReadings.getPm10SubIndex()));
 
-        labelPm25SubIndex.setText(superSubScript(getString(R.string.pm25_sub_index), SUBSCRIPT_TYPE_A));
-        progressPm25SubIndex.setProgress((int) Double.parseDouble(regionReadings.getPm25SubIndex()) * PERCENTAGE / VALUE_HAZARDOUS);
-        textPm25SubIndex.setText(regionReadings.getPm25SubIndex());
+        labelPm25.setText(superSubScript(getString(R.string.pm25_sub_index), SUBSCRIPT_TYPE_A));
+        setCurrentPsiLevel(textPm25Before, textPm25After, Float.parseFloat(regionReadings.getPm25SubIndex()));
 
-        labelO3SubIndex.setText(superSubScript(getString(R.string.o3_sub_index), SUBSCRIPT_TYPE_B));
-        progressO3SubIndex.setProgress((int) Double.parseDouble(regionReadings.getO3SubIndex()) * PERCENTAGE / VALUE_HAZARDOUS);
-        textO3SubIndex.setText(regionReadings.getO3SubIndex());
+        labelO3.setText(superSubScript(getString(R.string.o3_sub_index), SUBSCRIPT_TYPE_B));
+        setCurrentPsiLevel(textO3Before, textO3After, Float.parseFloat(regionReadings.getO3SubIndex()));
 
-        labelCoSubIndex.setText(getString(R.string.co_sub_index));
-        progressCoSubIndex.setProgress((int) Double.parseDouble(regionReadings.getCoSubIndex()) * PERCENTAGE / VALUE_HAZARDOUS);
-        textCoSubIndex.setText(regionReadings.getCoSubIndex());
+        labelCo.setText(getString(R.string.co_sub_index));
+        setCurrentPsiLevel(textCoBefore, textCoAfter, Float.parseFloat(regionReadings.getCoSubIndex()));
 
-        labelSo2SubIndex.setText(superSubScript(getString(R.string.so2_sub_index), SUBSCRIPT_TYPE_C));
-        progressSo2SubIndex.setProgress((int) Double.parseDouble(regionReadings.getSo2SubIndex()) * PERCENTAGE / VALUE_HAZARDOUS);
-        textSo2SubIndex.setText(regionReadings.getSo2SubIndex());
+        labelSo2.setText(superSubScript(getString(R.string.so2_sub_index), SUBSCRIPT_TYPE_C));
+        setCurrentPsiLevel(textSo2Before, textSo2After, Float.parseFloat(regionReadings.getSo2SubIndex()));
 
-        labelNo2SubIndex.setText(superSubScript(getString(R.string.no2_sub_index), SUBSCRIPT_TYPE_C));
-        progressNo2OneHourMax.setProgress((int) Double.parseDouble(regionReadings.getNo2OneHourMax()) * PERCENTAGE / VALUE_HAZARDOUS);
-        textNo2OneHourMax.setText(regionReadings.getNo2OneHourMax());
+        labelNo2.setText(superSubScript(getString(R.string.no2_sub_index), SUBSCRIPT_TYPE_C));
+        setCurrentPsiLevel(textNo2Before, textNo2After, Float.parseFloat(regionReadings.getNo2OneHourMax()));
+    }
+
+    private void setCurrentPsiLevel(TextView psiLevelBefore, TextView psiLevelAfter, float psi) {
+        float ratioBefore = psi / VALUE_HAZARDOUS;
+        float ratioAfter = (VALUE_HAZARDOUS - psi) / VALUE_HAZARDOUS;
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayoutCompat.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        params.weight = ratioAfter;
+        psiLevelBefore.setLayoutParams(params);
+
+        params = new LinearLayout.LayoutParams(
+                LinearLayoutCompat.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        params.weight = ratioBefore;
+        psiLevelAfter.setLayoutParams(params);
+
+        psiLevelAfter.setText("▲");
+    }
+
+    private int getHealthStatusDrawable(double psiLevel) {
+        if(psiLevel <= 100) {
+            return R.drawable.ic_thumb_up_white_24dp;
+        }
+
+        return R.drawable.ic_thumb_down_white_24dp;
     }
 
     private String determineStatus(double psiLevel) {
